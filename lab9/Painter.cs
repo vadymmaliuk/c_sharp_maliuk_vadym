ff0b4fa527ee7253f5477270ddcba6d85a508bd5
vadymmaliuk <72086467+vadymmaliuk@users.noreply.github.com>
2020-12-25 03:35:03 +0200

    Add files via upload

---
+++ lab9/Painter.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    static class Painter
+    {
+        public static void Draw(IDraw shape)
+        {
+            shape.Draw();
+        }
+    }
+}
---
+++ lab9/Shape.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Drawing;
+
+namespace ConsoleApp1
+{
+    abstract class Shape
+    { 
+        public abstract string Name { get; }
+        public abstract string FigureColor { get; set; }
+        public abstract int NumOfVertices { get; }
+        public abstract double Square();
+        public abstract double Perimeter();
+        protected string RandomColor()
+        {
+            Random randomGen = new Random();
+            KnownColor[] names = (KnownColor[])Enum.GetValues(typeof(KnownColor));
+            KnownColor randomColorName = names[randomGen.Next(names.Length)];
+            Color randomColor = Color.FromKnownColor(randomColorName);
+
+            return randomColor.Name;
+        }
+    }
+}
