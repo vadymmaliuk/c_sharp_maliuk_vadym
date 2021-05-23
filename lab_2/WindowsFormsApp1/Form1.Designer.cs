d7609028666f0393f4fa7e07d3880ff196e9c27c
vadymmaliuk <72086467+vadymmaliuk@users.noreply.github.com>
2021-05-23 21:30:36 +0300

    Add files via upload

---
+++ lab_2/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace OKnCANCEL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.XCoord = new System.Windows.Forms.Label();
+            this.YCoord = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.Crimson;
+            this.button1.Location = new System.Drawing.Point(9, 9);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Padding = new System.Windows.Forms.Padding(2, 0, 0, 0);
+            this.button1.Size = new System.Drawing.Size(31, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "❌";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.button2.Location = new System.Drawing.Point(271, 168);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 76);
+            this.button2.TabIndex = 0;
+            this.button2.TabStop = false;
+            this.button2.Text = "Спіймай мене";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // XCoord
+            // 
+            this.XCoord.AutoSize = true;
+            this.XCoord.Location = new System.Drawing.Point(634, 15);
+            this.XCoord.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.XCoord.Name = "XCoord";
+            this.XCoord.Size = new System.Drawing.Size(20, 13);
+            this.XCoord.TabIndex = 1;
+            this.XCoord.Text = "X: ";
+            // 
+            // YCoord
+            // 
+            this.YCoord.AutoSize = true;
+            this.YCoord.Location = new System.Drawing.Point(634, 39);
+            this.YCoord.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.YCoord.Name = "YCoord";
+            this.YCoord.Size = new System.Drawing.Size(17, 13);
+            this.YCoord.TabIndex = 2;
+            this.YCoord.Text = "Y:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 440);
+            this.Controls.Add(this.YCoord);
+            this.Controls.Add(this.XCoord);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CLICK \'OK\' BUTTON!";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label XCoord;
+        private System.Windows.Forms.Label YCoord;
+    }
+}
