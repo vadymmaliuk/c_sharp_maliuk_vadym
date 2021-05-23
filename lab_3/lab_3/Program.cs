d5c0da3291c4ae30727cc5d2fdaed7964e2d53a8
vadymmaliuk <72086467+vadymmaliuk@users.noreply.github.com>
2021-05-23 21:32:01 +0300

    Add files via upload

---
+++ lab_3/lab_3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboratory_3
+{
+    static class Program
+    {
+        /// <summary>
+        /// Головна точка в програму.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
