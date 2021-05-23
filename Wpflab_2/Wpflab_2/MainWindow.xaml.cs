8126b2c987e418c47f9bec5a9d4adfb56722028f
vadymmaliuk <72086467+vadymmaliuk@users.noreply.github.com>
2021-05-23 21:33:31 +0300

    Add files via upload

---
+++ Wpflab_2/Wpflab_2/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+
+
+namespace Wpflab_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ButtonsField bf = new ButtonsField(textBox2);
+            bf.drawField(gridForButtons);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox.Text != "")
+            {
+                comboBox.Items.Add(textBox.Text);
+                textBox.Text = "";
+            }
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (comboBox.Items.Count != 0)
+                comboBox.Items.RemoveAt(comboBox.Items.Count - 1);
+            else
+                MessageBox.Show("Що видаляти?", "Комбо Бокс пустий");
+        }
+    }
+}
